9c164211cb7a2f7bf99b257c450564f8464c8683
aiya000 <aiya000.develop@gmail.com>
2017-06-12 13:16:40 +0900

    Benrify（便利ファイ） Exception instances

--- src/Control/Exception/Throwable.hs
+++ src/Control/Exception/Throwable.hs
@@ -1,21 +1,39 @@
 module Control.Exception.Throwable
   ( IOException' (..)
+  , ioException'
   , IllegalArgumentException (..)
+  , illegalArgumentException
   ) where
 
 import Control.Exception.Safe (Exception)
+import Data.Typeable (Typeable)
 
 
 -- | Simular to @Control.Exception.IOException@
-data IOException' = IOException' { ioExceptionCause :: String }
-                  | FileNotFoundException { fileNotFoundCause :: String }
-  deriving (Read, Show)
+data IOException' a = IOException' { ioExceptionCause :: String, ioExceptionClue :: a }
+                    | FileNotFoundException { fileNotFoundCause :: String, fileNotFoundClue :: a }
 
-instance Exception IOException'
+instance Show a => Show (IOException' a) where
+  show (IOException' cause _) = "IOException': " ++ show cause
+  show (FileNotFoundException cause _) = "FileNotFoundException: " ++ show cause
+
+instance (Typeable a, Show a) => Exception (IOException' a)
+
+-- | A constructor for IOException' but doesn't take the clue
+ioException' :: String -> IOException' ()
+ioException' = flip IOException' ()
 
 
 -- Like java.lang.IllegalArgumentException
-data IllegalArgumentException = IllegalArgumentException { illegalArgumentCause :: String }
-  deriving (Read, Show)
+data IllegalArgumentException a = IllegalArgumentException { illegalArgumentCause :: String, illegalArgumentClue :: a }
 
-instance Exception IllegalArgumentException
+instance Show a => Show (IllegalArgumentException a) where
+  show (IllegalArgumentException cause  _) = "IllegalArgumentException: " ++ show cause
+
+instance (Typeable a, Show a) => Exception (IllegalArgumentException a)
+
+-- | A constructor for IllegalArgumentException but doesn't take the clue
+illegalArgumentException :: String -> IllegalArgumentException ()
+illegalArgumentException = flip IllegalArgumentException ()
+
+
