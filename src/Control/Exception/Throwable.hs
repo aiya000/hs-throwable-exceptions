add03f9a677c91b9fe15c3e90336113ef382f843
aiya000 <aiya000.develop@gmail.com>
2017-06-12 11:07:55 +0900

    Create src/Control/Exception/Throwable.hs

---
+++ src/Control/Exception/Throwable.hs
@@ -0,0 +1,20 @@
+module Control.Exception.Throwable
+  ( IOException' (..)
+  ) where
+
+import Control.Exception.Safe (Exception)
+
+
+-- | Simular to @Control.Exception.IOException@
+data IOException' = IOException' { ioExceptionCause :: String }
+                  | FileNotFoundException { fileNotFoundCause :: String }
+  deriving (Read, Show)
+
+instance Exception IOException'
+
+
+-- Like java.lang.IllegalArgumentException
+data IllegalArgumentException = IllegalArgumentException { illegalArgumentCause :: String }
+  deriving (Read, Show)
+
+instance Exception IllegalArgumentException
