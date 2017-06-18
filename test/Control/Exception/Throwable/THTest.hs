83cf503b8c840804955c5c951cff45147f334cbb
aiya000 <aiya000.develop@gmail.com>
2017-06-19 01:58:45 +0900

    Add test for the compile time

--- test/Control/Exception/Throwable/THTest.hs
+++ test/Control/Exception/Throwable/THTest.hs
@@ -1,12 +1,37 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Control.Exception.Throwable.THTest where
 
+import Control.Exception.Safe (Exception)
+import Control.Monad (void)
+import Data.Typeable (Typeable)
 import Test.DocTest (doctest)
 import Test.Tasty (TestTree)
 import Test.Tasty.HUnit (testCase, Assertion)
 import qualified Control.Exception.Throwable.TH as ETH
 
+ETH.declareException "THTestException"
+
+-- Indicate `e ()` is Exception instance in the compile time
+type IsException e = (Show (e ()), Typeable (e ()), Exception (e ())) => e ()
+
+
+refl :: IsException a
+refl = undefined
+
+through :: a -> Assertion
+through = void . return
+
 
 test_doctest :: [TestTree]
 test_doctest =
   [ testCase "can through TH.hs" $ doctest ["src/Control/Exception/Throwable/TH.hs"]
+  ]
+
+test_generated_datatype :: [TestTree]
+test_generated_datatype =
+  [ testCase "is Exception instance" $ through (refl :: IsException THTestException)
   ]
